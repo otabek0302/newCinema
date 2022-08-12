760e5e30efb78fc283673c3ef587d2dab2f82fa7
Otabek0302 <fenix_systems@MacBook-Pro.local>
2022-08-12 15:10:54 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,103 @@
+let URL = 'http://localhost:3001/'
+
+function update() {
+     axios.get(URL + 'movies')
+         .then(res => {
+             reload(res.data)
+            //  console.log(res.data);
+          })
+     .catch(err => console.log(err))
+}
+update()
+
+//Variables
+let ul = document.querySelector('.promo__interactive-list');
+let modal = document.querySelector('.modal');
+let promo__bg = document.querySelector('.promo__bg'),
+    promo__genre = document.querySelector('.promo__genre'),
+    promo__title = document.querySelector('.promo__title'),
+    promo__descr = document.querySelector('.promo__descr'),
+    imdb = document.querySelector('.imdb'),
+    movieSearch = document.querySelector('.movieSearch');
+
+
+//Serach
+let inpSearch = document.querySelector('#search')
+inpSearch.onkeyup = () => {
+    axios.get(URL + 'movies')
+        .then(res => {
+            let arr = res.data;
+            let filtered = arr.map((item) => {
+                let key = item.Title.toLowerCase();
+                let value = inpSearch.value.toLowerCase().trim()
+                if (key.includes(value)) {
+                    return item;
+                }
+            })
+            // reload(filtered)
+            console.log(filtered);
+        })
+     .catch(err => console.log(err))
+}
+
+
+//Sorted
+function reload(arr) {
+    ul.innerHTML = "";
+    for (const item of arr) {
+        let li = document.createElement('li'),
+            a = document.createElement('a'),
+            del = document.createElement('div'),
+            star = document.createElement('div'),
+            span = document.createElement('span');
+        
+            li.classList.add('promo__interactive-item'),
+            del.classList.add('delete'),
+            star.classList.add('star')    
+        
+            a.innerHTML = `${arr.indexOf(item) + 1}. ${item.Title}`
+        
+            star.append(span)
+            li.append(star,a,del)
+            ul.append(li);
+
+        
+            li.onclick = () => {
+                //Function change showmovies[index]
+                location.href = `./post.html?id=${item.id}`
+                console.log(item);
+            }
+            del.onclick = () => {
+                delItem(item)
+            } 
+    }
+}
+
+
+//Function del
+function delItem (data) {    
+    let idx = movies.indexOf(data)
+    movies.splice(idx, 1)
+    reload(movies)
+    console.log(movies);
+}
+
+//Meny activity and Filter by Genres
+let promo__menu_item = document.querySelectorAll('.promo__menu-item');
+promo__menu_item.forEach(item => {
+    item.onclick = () => {
+        promo__menu_item.forEach(el => el.classList.remove('promo__menu-item_active'))
+        item.classList.add('promo__menu-item_active')
+        
+        //Outputing with Type
+        let type = item.getAttribute('data-type')
+        promo__menu_item.filter(el => {
+            if (el.type.toLowerCase() == type.toLowerCase()) {
+                let genre = [el];
+                reload(genre)
+                console.log(genre);
+            }
+        });
+    }
+})
+
